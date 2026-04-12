0303d3082e2898e32c6b9da355b53a1c4fe36799
SirNacho <steven.frausto@nachoweb.tech>
2026-04-12 01:12:36 -0500

    Added libraries in lib as submodules and adjusted cmake to use the libraries. src/main.cpp will check if the libraries are intialized.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
+//External Libraries
+#include <SDL3/SDL.h>
+#include <glm/glm.hpp>
+
 int main()
 {
-    std::cout << "Hello World!";
+    if (!SDL_Init(SDL_INIT_VIDEO)) 
+    {
+        std::cerr << "SDL3 failed to intialize: " << SDL_GetError() << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::cout << "SDL3 has intialized!" << std::endl;
 
+    SDL_Quit();
     return EXIT_SUCCESS;
 }
